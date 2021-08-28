113b2bc2de828192a14c49ca106b094e97743b59
nick <nick.linuxedu@gmail.com>
2021-08-28 17:40:23 -0500

    ansible script added

--- scripts/ping_django_via_ansible.sh
+++ scripts/ping_django_via_ansible.sh
@@ -1,5 +1,5 @@
 #! /bin/bash
 
 chmod 600 ../terraform/aws_private_key/aws_private_key.txt
-(cd ../ansible/ansible-dynamic-aws-inventory/; ansible-playbook --private-key ../../terraform/aws_private_key/aws_private_key.txt -i django_aws_ec2.yaml ping.yaml)
+(cd ../ansible/ping-dynamic/; ansible-playbook --private-key ../../terraform/aws_private_key/aws_private_key.txt -i django_aws_ec2.yaml ping.yaml)
 exec bash

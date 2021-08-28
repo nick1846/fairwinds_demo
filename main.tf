259b17afdf3efb1f704e9f58cb9c17f0b5c42103
nick <nick.linuxedu@gmail.com>
2021-08-28 10:47:47 -0500

    ansible added

--- main.tf
+++ main.tf
@@ -4,10 +4,21 @@ provider "aws" {
   # secret_key = var.aws_secret_key
 }
 
-resource "aws_key_pair" "ec2-user-public" {
-  key_name   = var.my_key_name
-  public_key = var.my_publickey
+resource "tls_private_key" "key" {
+  algorithm = "RSA"
+  rsa_bits  = 4096
 }
+
+resource "aws_key_pair" "aws_key" {
+  key_name   = var.my_key_name
+  public_key = tls_private_key.key.public_key_openssh
+}
+
+
+# resource "aws_key_pair" "ec2-user-public" {
+#   key_name   = var.my_key_name
+#   public_key = var.my_publickey
+# }
 
 resource "aws_eip" "django_server" {
   count    = var.eip_count
@@ -64,3 +75,27 @@ output "django_server_eip" {
   value       = aws_eip.django_server.*.public_ip
 }
 
+resource "null_resource" "provision_django" {
+
+  connection {
+    host        = element(module.my_ec2.*.public_ip, 0)
+    type        = "ssh"
+    user        = "ec2-user"
+    private_key = tls_private_key.key.private_key_pem
+
+  }
+  
+  provisioner "file" {
+    source      = "ansible/playbooks"
+    destination = "/home/ec2-user"
+  }
+
+  provisioner "remote-exec" {
+
+    inline = [
+      "ansible-playbook /home/ec2-user/playbooks/django_server.yaml",
+    ]
+  }
+
+
+}
--- userdata.sh
+++ userdata.sh
@@ -11,25 +11,7 @@ python3 -m pip install --user --upgrade pip
 python3 -m pip install ansible
 
 
-# docker run -it --rm --user "$(id -u):$(id -g)" -v "$PWD":/usr/src/app -w /usr/src/app django django-admin.py startproject mysite
-# echo 'ALLOWED_HOSTS = ['*']' >> /home/ec2-user/mysite/mysite/settings.py
 
-# cat <<EOF > /home/ec2-user/mysite/Dockerfile
-# FROM python:3.9.5-slim
-# ENV PYTHONUNBUFFERED 1
-# RUN mkdir /code
-# WORKDIR /code
-# COPY requirements.txt /code/
-# RUN pip install --user -r requirements.txt
-# COPY . /code/
-# CMD python manage.py runserver 0.0.0.0:8000
-# EOF
-
-# echo "Django==3.2" > /home/ec2-user/mysite/requirements.txt
-
-# docker build -t my-django-image /home/ec2-user/mysite/.
-
-# docker run --name django-app -p 80:8000 -d my-django-image
 
 
 
